796be1fe3587a0d28a1e9f4e96f1b9184d117e33
Dzmitry Fedchanka <demolior777@icloud.com>
2022-07-04 13:22:13 +0300

    Initial Commit

---
+++ DubDubGrub/DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  DubDubGrubApp.swift
+//  DubDubGrub
+//
+//  Created by Źmicier Fiedčanka on 4.07.22.
+//
+
+import SwiftUI
+
+@main
+struct DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
