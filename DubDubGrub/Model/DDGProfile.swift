56ab032ac4988c6603c0bc3a9f88f0958dc428a2
Dzmitry Fedchanka <demolior777@icloud.com>
2022-07-07 10:53:30 +0300

    Add Profile Model as in CloudKit

---
+++ DubDubGrub/Model/DDGProfile.swift
@@ -0,0 +1,36 @@
+//
+//  DDGProfile.swift
+//  DubDubGrub
+//
+//  Created by Źmicier Fiedčanka on 7.07.22.
+//
+
+import CloudKit
+
+struct DDGProfile {
+    
+    static let kFirstName   = "firstName"
+    static let kLastName    = "lastName"
+    static let kAvatar      = "avatar"
+    static let kBio         = "bio"
+    static let kCompanyName = "companyName"
+    static let kIsCheckedIn = "isCheckedIn"
+    
+    let ckRecordID: CKRecord.ID
+    let firstName: String
+    let lastName: String
+    let avatar: CKAsset!
+    let bio: String
+    let companyName: String
+    let isCheckedIn: CKRecord.Reference? = nil
+    
+    init(record: CKRecord) {
+        ckRecordID  = record.recordID
+        firstName   = record[DDGProfile.kFirstName] as? String ?? "N/A"
+        lastName    = record[DDGProfile.kLastName] as? String ?? "N/A"
+        avatar      = record[DDGProfile.kAvatar] as? CKAsset
+        bio         = record[DDGProfile.kBio] as? String ?? "N/A"
+        companyName = record[DDGProfile.kCompanyName] as? String ?? "N/A"
+    }
+}
+ 
