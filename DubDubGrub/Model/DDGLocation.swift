4905ddff5cc3f06f8ecada7ab4ffa0598186442a
Dzmitry Fedchanka <demolior777@icloud.com>
2022-07-07 10:42:43 +0300

    Add Location Model as in CloudKit

---
+++ DubDubGrub/Model/DDGLocation.swift
@@ -0,0 +1,42 @@
+//
+//  DDGLocation.swift
+//  DubDubGrub
+//
+//  Created by Źmicier Fiedčanka on 7.07.22.
+//
+
+import CloudKit
+
+struct DDGLocation {
+    
+    static let kName        = "name"
+    static let kDescription = "description"
+    static let kSquareAsset = "squareAsset"
+    static let kBannerAsset = "bannerAsset"
+    static let kAddress     = "address"
+    static let kLocation    = "location"
+    static let kWebsite     = "website"
+    static let kPhone       = "phone"
+    
+    let ckRecordID: CKRecord.ID
+    let name: String
+    let description: String
+    let squareAsset: CKAsset!
+    let bannerAsset: CKAsset!
+    let address: String
+    let location: CLLocation
+    let website: String
+    let phone: String
+    
+    init(record: CKRecord) {
+        ckRecordID  = record.recordID
+        name        = record[DDGLocation.kName] as? String ?? "N/A"
+        description = record[DDGLocation.kDescription] as? String ?? "N/A"
+        squareAsset = record[DDGLocation.kSquareAsset] as? CKAsset
+        bannerAsset = record[DDGLocation.kBannerAsset] as? CKAsset
+        address     = record[DDGLocation.kAddress] as? String ?? "N/A"
+        location    = record[DDGLocation.kLocation] as? CLLocation ?? CLLocation(latitude: 0, longitude: 0)
+        website     = record[DDGLocation.kWebsite] as? String ?? "N/A"
+        phone       = record[DDGLocation.kPhone] as? String ?? "N/A"
+    }
+}
